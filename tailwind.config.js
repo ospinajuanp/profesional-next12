718748fa6e3a9a1867c058737276ce562d6c2b08
Ospinajuanp <juanpo996@gmail.com>
2023-02-15 16:02:10 -0500

    Agregando tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    colors: {
+      ...colors,
+    },
+  },
+};
