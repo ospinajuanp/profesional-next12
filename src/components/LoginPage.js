ac7cb3f067e24ca6c3e503355b058275a84e2e0b
Ospinajuanp <juanpo996@gmail.com>
2023-02-17 16:40:42 -0500

    change ext jsx -> js all new file

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -9,14 +9,14 @@ return (
             <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
         </div>
-            <Form className="mt-8 space-y-6" action="#" method="POST">
+            <form className="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="rounded-md shadow-sm -space-y-px">
                 <div>
                 <label htmlFor="email-address" className="sr-only">
                     Email address
                 </label>
-                <Field
+                <input
                     id="email-address"
                     name="email"
                     type="email"
@@ -30,7 +30,7 @@ return (
                 <label htmlFor="password" className="sr-only">
                     Password
                 </label>
-                <Field
+                <input
                     id="password"
                     name="password"
                     type="password"
@@ -68,7 +68,7 @@ return (
                 Sign in
                 </button>
             </div>
-            </Form>
+            </form>
         </div>
     </div>
     </>
